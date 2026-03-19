a95e97f26eec55d6b0b7e90372581c56b412512c
sivinhdzj-cell <sivinhdzj@gmail.com>
2026-03-19 17:17:47 +0700

    Initial commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using sivinh_2122110368.Models;
+
+namespace sivinh_2122110368.Data
+{
+    public class AppDbContext : DbContext
+    {
+        // Constructor nhận connection string từ Program.cs
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; } = null !;
+    }
+} 
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using sivinh_2122110368.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// 🔥 Kết nối SQL Server
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
